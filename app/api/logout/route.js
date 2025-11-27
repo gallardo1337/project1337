5731a2006341165c5b2b4837d0823f187501af1e
gallardo1337 <kevinharasim@web.de>
2025-11-27 23:32:33 +0100

    Update route.js

--- app/api/logout/route.js
+++ app/api/logout/route.js
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 
 export async function POST() {
   const res = NextResponse.json({ ok: true });
+  // Cookie löschen (falls gesetzt)
   res.cookies.set("auth_1337", "", {
     httpOnly: true,
     sameSite: "strict",
