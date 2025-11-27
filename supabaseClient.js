3c0573e5090ec3e788fda394b97455061e1efae4
gallardo1337 <kevinharasim@web.de>
2025-11-27 01:35:24 +0100

    Delete supabaseClient.js

--- supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
-
-export const supabase = createClient(
-  window.env.NEXT_PUBLIC_SUPABASE_URL,
-  window.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-);
