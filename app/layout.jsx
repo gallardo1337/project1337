1ddb844f79ade8609fd4129678a56fa4f6351909
gallardo1337 <kevinharasim@web.de>
2025-11-29 02:11:22 +0100

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "1337 Film-Bibliothek",
+  title: "1337 Library",
   description: "Eigene Film-Bibliothek mit Tags, Actors und Supabase"
 };
 
