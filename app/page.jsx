83d166eec495a2360ab253ebde8dd03bcf64a107
gallardo1337 <kevinharasim@web.de>
2025-12-27 19:50:10 +0100

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1153,7 +1153,7 @@ export default function HomePage() {
           margin: 0;
           text-align: center;
           display: -webkit-box;
-          -webkit-line-clamp: 2;
+          -webkit-line-clamp: 1;
           -webkit-box-orient: vertical;
           overflow: hidden;
           min-height: 18px;
