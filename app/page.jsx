7905b0a1bd291225318125da302d94c34907ba37
gallardo1337 <kevinharasim@web.de>
2025-12-27 14:16:34 +0100

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1027,7 +1027,7 @@ export default function HomePage() {
         .card__title {
           font-weight: 500;
           font-size: 14px;
-          line-height: 1;
+          line-height: 1.4;
           letter-spacing: -0.01em;
           margin: 0;
           text-align: center;
